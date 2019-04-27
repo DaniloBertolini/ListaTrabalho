fd606844ffc52126a57ee08d92060943a92e7b55
DaniloBertolini <49735033+DaniloBertolini@users.noreply.github.com>
2019-04-26 22:02:52 -0300

    Delete Exercício6.py

--- Exercício6.py
+++
@@ -1,6 +0,0 @@
-distancia = int(input("Qual a distância a ser percorrida: "))
-velocidade = int(input("Qual a velocidade média esperada: "))
-
-tempo = distancia / velocidade
-
-print("A duração da viagem é {0:3.1f}".format (tempo),"horas")
