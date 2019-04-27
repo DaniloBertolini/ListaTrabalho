6b37b9deb6e7a1b7d0e5da0e52f049856e48e09b
DaniloBertolini <49735033+DaniloBertolini@users.noreply.github.com>
2019-04-26 22:02:38 -0300

    Delete Exercício4.py

--- Exercício4.py
+++
@@ -1,8 +0,0 @@
-salario = int(input("Digite seu salario atual: "))
-aumento = int(input("Digite a porcentagem do aumento: "))
-
-aumentosalario = salario * aumento / 100
-salarionovo = salario + aumentosalario
-
-print("O aumento do salário é:", aumentosalario)
-print("Seu novo salário é:", salarionovo)
